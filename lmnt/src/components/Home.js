478fff8940f5fbb8341b6c80b7f772b7d471f7a5
Lindokuhle Shabalala <mlixhaba@gmail.com>
2021-07-20 18:53:38 +0200

    added chat components

--- lmnt/src/components/Home.js
+++ lmnt/src/components/Home.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import '../App.css';
-
-import HeroSection from './HeroSection';
+import './Home.css';
+import React /*{useState}*/ from 'react';
+import Sidebar from './Sidebar';
+import Chat from './Chat';
+// import Login from './Login';
+import { BrowserRouter as Router,Switch, Route} from 'react-router-dom';
+// import {useStateValue} from './StateProvider';
 
 
 function Home() {
+  // const [{user}, dispatch] = useStateValue();
   return (
-    <>
-      <HeroSection />
-    </>
+    <div className="app_body">
+      <Router>
+        <Sidebar/>
+        <Switch>
+          <Route path="/rooms/:roomId">
+            <Chat/>
+          </Route>
+          <Route path="/">
+            <Chat/>
+          </Route>              
+        </Switch>            
+      </Router>
+    </div>
+
   );
 }
+
 
 export default Home;
