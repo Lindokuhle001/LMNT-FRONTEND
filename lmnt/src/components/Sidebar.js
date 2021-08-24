715b30767b40f73b0dfe02b7128d26852d6cadf2
Lindokuhle Shabalala <mlixhaba@gmail.com>
2021-08-24 17:28:42 +0200

    changed names to first names

--- lmnt/src/components/Sidebar.js
+++ lmnt/src/components/Sidebar.js
@@ -2,17 +2,16 @@ import { React, useState, useEffect } from "react";
 import "./Sidebar.css";
 import SidebarChat from "./SidebarChat";
 import db from "../firebase";
-import { useStateValue } from '../StateProvider';
-import {Avatar} from "@material-ui/core";
+import { useStateValue } from "../StateProvider";
+import { Avatar } from "@material-ui/core";
 
 function Sidebar(props) {
   const [rooms, setRooms] = useState([]);
+  // const [chatName, setChatName] = useState([]);
   // eslint-disable-next-line no-unused-vars
-  const [{user},dispatch] = useStateValue();
+  const [{ user }, dispatch] = useStateValue();
 
   useEffect(() => {
-
-
     const unsubscribe = db.collection("rooms").onSnapshot((snapshot) =>
       setRooms(
         snapshot.docs
@@ -21,25 +20,24 @@ function Sidebar(props) {
             data: doc.data(),
           }))
           .filter((a) => {
-            // console.log(a.data.users.includes(user.displayName))
-            return a.data.users.includes(user.displayName)})
+            return a.data.users.includes(user.displayName);
+          })
       )
     );
-    // console.log(a.data.users)
-// .includes(user.displayName)
+
     return () => {
       unsubscribe();
     };
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <div className="sidebar">
       <div className="sidebar_header">
-      <h1>{user.displayName}</h1>
+        <h1>{user.displayName.split(' ').shift()}</h1>
 
-                <Avatar src={user?.photoURL}/>
-            </div>
+        <Avatar src={user?.photoURL} />
+      </div>
       {/* <div className="sidebar_search">
                 <div className="sidebar_searchContainer">
                     <SearchOutlined />
@@ -49,7 +47,15 @@ function Sidebar(props) {
       <div className="sidebar_chats">
         <SidebarChat addNewChat />
         {rooms.map((room) => (
-          <SidebarChat key={room.id} id={room.id} name={room.data.name} />
+          <SidebarChat
+            key={room.id}
+            id={room.id}
+            name={room.data.users
+              .filter((myUser) => myUser !== user)
+              .pop()
+              .split(" ")
+              .shift()}
+          />
         ))}
       </div>
     </div>
