e67c57b677ec2b8ff1d66ae645ac900a0f5e1986
Lindokuhle Shabalala <mlixhaba@gmail.com>
2021-08-27 23:20:46 +0200

    added unmatch cross

--- lmnt/src/components/SidebarChat.js
+++ lmnt/src/components/SidebarChat.js
@@ -4,10 +4,12 @@ import "./SidebarChat.css";
 import { Link } from "react-router-dom";
 import db from "../firebase";
 import { useStateValue } from "../StateProvider";
+import { Button } from "./Button";
 
 function SidebarChat({ id, name, addNewChat }) {
-//   const [seed, setSeed] = useState("");
+  //   const [seed, setSeed] = useState("");
   const [messages, setMessages] = useState("");
+  const [toggle, setToggle] = useState(true);
   const [match, setMatch] = useState("");
   // eslint-disable-next-line no-unused-vars
   const [{ user }, dispatch] = useStateValue();
@@ -24,9 +26,9 @@ function SidebarChat({ id, name, addNewChat }) {
     }
   }, [id]);
 
-//   useEffect(() => {
-//     setSeed(Math.floor(Math.random() * 5000));
-//   }, []);
+  //   useEffect(() => {
+  //     setSeed(Math.floor(Math.random() * 5000));
+  //   }, []);
   // add new match
   const createChat = () => {
     db.collection("users").onSnapshot((snapshot) => {
@@ -42,9 +44,34 @@ function SidebarChat({ id, name, addNewChat }) {
       db.collection("rooms").add({
         name: match,
         users: [match, user.displayName],
+        visible: true,
       });
     }
   };
+
+  function unMatch(identity) {
+    //  console.log(id);
+
+    db.collection("rooms").doc(identity).update({
+      users: [],
+    });
+  }
+
+  function unMatchToggle(identity) {
+    console.log(identity);
+    setToggle(() => {
+      if (toggle === false) {
+        // console.log(toggle);
+        return true;
+      }
+      return toggle === false;
+    });
+    console.log(id);
+    // db.collection("rooms").doc(id).update({
+    //     visible: toggle,
+    //   });
+  }
+  // onClick={unMatch(id)}
 
   return !addNewChat ? (
     <Link to={`/home/rooms/${id}`} key={id}>
@@ -54,11 +81,37 @@ function SidebarChat({ id, name, addNewChat }) {
           <h3>{name}</h3>
           <p>{messages[0]?.message}</p>
         </div>
+
+        <button
+          className={
+            toggle === true ? "unMatch_button_on" : "unMatch_button_off"
+          }
+          type="submit"
+          onClick={() => {
+            unMatch(id);
+            console.log(id);
+          }}
+        >
+          X
+        </button>
       </div>
     </Link>
   ) : (
-    <div onClick={createChat} className="sidebarChat">
-      <button className="add-new-chat-title">Get New Match</button>
+    <div className="sidebarChat">
+      <button onClick={createChat} className="add-new-chat-title">
+        Get New Match
+      </button>
+          
+        {console.log('id')}
+      <button
+        className="add-new-chat-title"
+        onClick={() => {
+          unMatchToggle(id);
+          console.log(id);
+        }}
+      >
+        Remove Match
+      </button>
     </div>
   );
 }
