388a536a876e870ad509293e9917f90e65532bd9
Lindokuhle Shabalala <mlixhaba@gmail.com>
2021-08-31 15:43:35 +0200

    finished styling profile

--- lmnt/src/components/Profile.js
+++ lmnt/src/components/Profile.js
@@ -4,7 +4,6 @@ import { useStateValue } from "../StateProvider";
 import db from "../firebase";
 import { Link } from "react-router-dom";
 
-
 fetch("https://opentdb.com/api.php?amount=50&category=12")
   .then((res) => res.json())
   .then((data) => console.log(data));
@@ -23,8 +22,8 @@ function Profile() {
     console.log(user);
   }
 
-  function setButtons(gender){
-    let xty = gender
+  function setButtons(gender) {
+    let xty = gender;
     console.log(xty);
   }
 
@@ -32,63 +31,87 @@ function Profile() {
     <div className="container">
       <div className="content">
         <form action="#">
-          <div class="user-details"></div>
-          <label htmlFor="userGender">I'm a</label>
-          <select name="gender" id="gender">
-            <option value="male" onClick={setButtons('male')}>male</option>
-            <option value="female">female</option>
-            <option value="other">other</option>
-          </select>
-          <label htmlFor="perferedGender">I'm looking for a</label>
-          <select name="gender" id="gender">
-            <option value="male">male</option>
-            <option value="female">female</option>
-            <option value="other">other</option>
-          </select>
-          <label htmlFor="gender">I'm x years old</label>
-          <select name="gender" id="gender">
-            <option value="age">all</option>
-            <option value="age">18</option>
-            <option value="age">19</option>
-            <option value="age">21</option>
-            <option value="age">22</option>
-            <option value="age">23</option>
-            <option value="age">24</option>
-            <option value="age">25</option>
-            <option value="age">26</option>
-            <option value="age">27</option>
-            <option value="age">28</option>
-            <option value="age">29</option>
-            <option value="age">30</option>
-          </select>
-          <label htmlFor="personalityType">My personalityType</label>
-          <select name="personalityType" id="personalityType">
-            <option value="ISTJ">ISTJ</option>
-            <option value="ISTP">ISTP</option>
-            <option value="ISFJ">ISFJ</option>
-            <option value="ISFP">ISFP</option>
-            <option value="INFJ">INFJ</option>
-            <option value="INFP">INFP</option>
-            <option value="INTJ">INTJ</option>
-            <option value="INTP">INTP</option>
-            <option value="ESTP">ESTP</option>
-            <option value="ESTJ">ESTJ</option>
-            <option value="ESFP">ESFP</option>
-            <option value="ESFJ">ESFJ</option>
-            <option value="ENFP">ENFP</option>
-            <option value="ENFJ">ENFJ</option>
-            <option value="ENTP">ENTP</option>
-            <option value="ENTJ">ENTJ</option>
-          </select>
-          <div className="button">
-            <input type="submit" value="Continue" />
+          <div className="profile-text">
+            <h4>You need to set up your profile before we can find you a match </h4>
+            <h4>
+              You will also need to know what your personality type is, If you
+               don't already know you can find out <a href="https://www.16personalities.com/free-personality-test">here</a>
+               {/*<span><Link to={`/home`}>
+            <button
+              className="add-new-chat-title"
+            >
+              here 
+            </button>
+          </Link>           
+          </span> */}
+            </h4>
+          </div>
+          <div class="user-details">
+            <label htmlFor="userGender">I'm a</label>
+            <select name="gender" id="gender">
+              <option value="male" onClick={setButtons("male")}>
+                male
+              </option>
+              <option value="female">female</option>
+              <option value="other">other</option>
+            </select>
+          </div>
+          <div class="user-details">
+            <label htmlFor="perferedGender">I'm looking for a</label>
+            <select name="gender" id="gender">
+              <option value="male">male</option>
+              <option value="female">female</option>
+              <option value="other">other</option>
+            </select>
+          </div>
+          <div class="user-details">
+            <label htmlFor="gender">I'm x years old</label>
+            <select name="gender" id="gender">
+              <option value="age">all</option>
+              <option value="age">18</option>
+              <option value="age">19</option>
+              <option value="age">21</option>
+              <option value="age">22</option>
+              <option value="age">23</option>
+              <option value="age">24</option>
+              <option value="age">25</option>
+              <option value="age">26</option>
+              <option value="age">27</option>
+              <option value="age">28</option>
+              <option value="age">29</option>
+              <option value="age">30</option>
+            </select>
+          </div>
+          <div class="user-details">
+            <label htmlFor="personalityType">My personalityType</label>
+            <select name="personalityType" id="personalityType">
+              <option value="ISTJ">ISTJ</option>
+              <option value="ISTP">ISTP</option>
+              <option value="ISFJ">ISFJ</option>
+              <option value="ISFP">ISFP</option>
+              <option value="INFJ">INFJ</option>
+              <option value="INFP">INFP</option>
+              <option value="INTJ">INTJ</option>
+              <option value="INTP">INTP</option>
+              <option value="ESTP">ESTP</option>
+              <option value="ESTJ">ESTJ</option>
+              <option value="ESFP">ESFP</option>
+              <option value="ESFJ">ESFJ</option>
+              <option value="ENFP">ENFP</option>
+              <option value="ENFJ">ENFJ</option>
+              <option value="ENTP">ENTP</option>
+              <option value="ENTJ">ENTJ</option>
+            </select>
           </div>
           <Link to={`/home`}>
-            <div onClick={addUser} className="sidebarChat">
-              <button className="add-new-chat-title">add</button>
-            </div>
+            <button
+              onClick={addUser}
+              className="add-new-chat-title"
+              id="profile-btn"
+            >
+              add
+            </button>
           </Link>
-          ;
         </form>
       </div>
     </div>
